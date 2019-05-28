c6b8cd066b5b43e544a1d4d67c8c1499a6e2379a
karla galvez <karla.galvez.m@hotmail.com>
2019-05-27 19:05:51 -0500

    subiendo todo

---
+++ programa3.js
@@ -0,0 +1,9 @@
+let nombre = "morita"
+let contrasena = "1234"
+
+if (nombre === "morita" && contrasena === "123") { // Si nombre === "morita" && contrasena === "123" ENTONCES 
+   console.log("abrir") //ESCRIBE abrir
+}   else { //SINO
+   
+   console.log("incorrecto") // ESCRIBE incorrecto
+}
